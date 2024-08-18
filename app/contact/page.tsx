20da8cefd3c9b47500017d9403da3c37cda01e72
Andy Nugraha Putra <andybima6@gmail.com>
2024-08-19 00:44:20 +0700

    contact page screen

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,96 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectValue, SelectLabel, SelectTrigger } from "@/components/ui/select";
+import { motion } from "framer-motion";
+import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa";
 
-const contact = () => {
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "(+62) 812 1678 5571",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "andybima6@gmail.com",
+  },
+  {
+    icon: <FaMapMarkedAlt />,
+    title: "Address",
+    description: "Malang, Jalan Kodok Loncat 12",
+  },
+];
+
+const Contact = () => {
   return (
-    <div>contact page </div>
-  )
-}
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+        
+          {/* Form */}
+          <div className="xl:h-[54px] order-2 xl:order-none">
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+              <h3 className="text-4xl text-accent">Let’s work together</h3>
+              <p className="text-white/60">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo odio reiciendis blanditiis ratione harum!
+              </p>
+              {/* Input */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <Input type="text" placeholder="Firstname" />
+                <Input type="text" placeholder="Lastname" />
+                <Input type="email" placeholder="Email Address" />
+                <Input type="tel" placeholder="Phone Number" />
+              </div>
+              {/* Select */}
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select a service" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select a service</SelectLabel>
+                    <SelectItem value="web">Web Development</SelectItem>
+                    <SelectItem value="uiux">UI/UX Design</SelectItem>
+                    <SelectItem value="logo">Logo Design</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              {/* TextArea */}
+              <Textarea className="h-[200px]" placeholder="Type your message here." />
+              {/* Button */}
+              <Button size="md" className="max-w-[200px]">
+                Send Message
+              </Button>
+            </form>
+          </div>
+            {/* Info */}
+            <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => (
+                <li key={index} className="flex items-center gap-6">
+                  <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                    <div className="text-[28px]">{item.icon}</div>
+                  </div>
+                  <div className="flex-1">
+                    <p className="text-white/60">{item.title}</p>
+                    <h3 className="text-xl">{item.description}</h3>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
+};
 
-export default contact
+export default Contact;
